a2f3c628dbd94ae146c173a957dbfe07a27fc318
Oscar Perez <ooscar.perez@protonmail.com>
2021-04-07 02:29:45 +0200

    zsh-vim: fix submodules and add missing plugins

---
+++ zsh/plugins.zsh
@@ -0,0 +1,8 @@
+# Install plugins manually
+# Not using oh-my-zsh, as it seems more easy this way
+source ~/.zsh/plugins/zsh-256color/zsh-256color.plugin.zsh
+source ~/.zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/.zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# FIXME: resolve it by looking at github ohmyzsh
+# source ~/.zsh/plugins/zsh-completions/zsh-completions.zsh
