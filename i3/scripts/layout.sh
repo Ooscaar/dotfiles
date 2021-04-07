06ac9a9871a7859e76aba0d8762fdb799f13b4c5
Oscar Perez <ooscar.perez@protonmail.com>
2021-04-07 11:47:21 +0200

    i3-polybar: fix problems

--- i3/scripts/layout.sh
+++ i3/scripts/layout.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+if xrandr | grep -q 'HDMI-1 connected'; then
+	xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+elif xrandr | grep -q 'HDMI-2 connected'; then
+	xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+else
+	echo "We do nothing"
+fi
+exit 0
