f1860c1563fe406e8f77ca2b843bccf2c4beb398
Oscar Perez <ooscar.perez@protonmail.com>
2021-04-10 13:40:48 +0200

    i3: add layout configuration for display port and laptop undocked

--- i3/scripts/layout.sh
+++ i3/scripts/layout.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-if xrandr | grep -q 'HDMI-1 connected'; then
+if [ "$1" == "-f" ]; then
+	# Laptop default configuration
+	# In case we are undocking the laptop
+	xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output HDMI-2 --off
+	exit 0
+elif xrandr | grep -q 'HDMI-1 connected'; then
 	xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output HDMI-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+	exit 0 
 elif xrandr | grep -q 'HDMI-2 connected'; then
 	xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-2 --off --output HDMI-2 --primary --mode 2560x1440 --pos 0x0 --rotate normal
+	exit 0
+elif xrandr | grep -q 'DP-1 connected'; then
+	xrandr --output eDP-1 --off --output DP-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal --output HDMI-1 --off --output HDMI-2 --off
+	exit 0
 else
 	echo "We do nothing"
+	exit 0
 fi
 exit 0
